d26dceb52f11f1f4b997f12d18297db0d0494320
nasetin <sasuhujanen@outlook.com>
2023-03-26 13:37:57 +0300

    onkotähäpakkokirjottaa?

---
+++ 23.3. itsenäinen tehtävä/23.3. itsenäinen tehtävä/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._3.itsenäinen_tehtävä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Pinta-ala laskuri");
+			Console.Write("Syötä Leveys metreinä: ");
+			string width = Console.ReadLine();
+			int userwidth = int.Parse(width);
+
+
+			Console.Write("Syötä korkeus metreinä: ");
+			string height = Console.ReadLine();
+			int userheight = int.Parse(height);
+
+			int surfacearea = userheight * userwidth;
+			Console.WriteLine("Pinta-ala= " + surfacearea + "m2");
+
+		}
+	}
+}
