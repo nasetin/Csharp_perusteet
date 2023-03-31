13401bb2e9e3fc57ccf809264c5eb387d47fb51e
nasetin <sasuhujanen@outlook.com>
2023-03-31 18:31:44 +0300

    31.3.tulostus tehtävä

---
+++ 26 tulosta luvut/26 tulosta luvut/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_tulosta_luvut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Valitse kokonais luku: ");            
+                int userInput = int.Parse(Console.ReadLine());
+                int a = 1;
+ 
+            while (a <= userInput)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
