fbce01aa598141da6f563b3a26c6149dd6c694f9
nasetin <sasuhujanen@outlook.com>
2023-04-03 19:54:15 +0300

    3.4 lähipäivä

---
+++ 16 Valinta Kuukaudet yhdessä 3.4/16 Valinta Kuukaudet yhdessä 3.4/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Valinta_Kuukaudet_yhdessä_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tee ohjelma, joka kysyy kuukauden ja tulostaa mihin vuoden aikaan se kuuluu.
+            // 11-12-1-2-3 ovat talvea. 4-5 ovat kevättä. 6-7-8 ovat kesää.
+            // 9-10 ovat syksyä.
+            //Kuukauden voi antaa numerona tai tekstinä.
+            //Suorita sovellus: if-else, switch ja ternary rakenteilla.
+            /*
+            Console.WriteLine("Syötä kuukausi numero 1-12:");
+            int userMonth = int.Parse(Console.ReadLine());
+            
+            if (userMonth == 11  || userMonth == 12 || userMonth == 1 || userMonth == 2 || userMonth == 3)
+            {
+                Console.WriteLine("talvi!");
+            }
+            else if (userMonth == 4 || userMonth == 5)
+            {
+                Console.WriteLine("Kevät!");
+            }
+            else if (userMonth == 6 || userMonth == 7 || userMonth == 8)
+            {
+                Console.WriteLine("Kesä!");
+            }
+            else if (userMonth == 9 || userMonth == 10)
+            {
+                Console.WriteLine("Syksy!");
+            }
+            else
+            {
+                Console.WriteLine("Väärä syöte. 1-12 numerot käytössä");
+            } 
+            */
+            // Muokataan teksi pieniksi kirjaimiksi
+
+            // !!!!!!!!!!!!!!monthIF = monthIF.ToLower();  ToLower() muokkaa kirjaimet pieniksi!!!!!!!!!!!!!!!!!!!
+            // toinen tapa tehää string esim. mothIF == "tammikuu".........
+            // -----------------------------------------------------------------------------------------------------
+            // Switch
+            /*
+            !!!!!!!!!!! case int xyz when (xyz >= 6 || xyz <= 8): 6 ja 8ssä kesä!!!!!!!!!!!!
+            switch (userMonth)
+                {
+                case 1:
+                case 2:
+                case 3:
+                case 12:
+                case 11:                 
+                    Console.WriteLine("Talvi");
+                    break;
+                 
+           
+                case 4:
+                case 5:
+                    Console.WriteLine("Kevät");
+                    break;
+                  
+            
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Kesä");
+                    break;
+           
+                case 9:
+                case 10:
+                    Console.WriteLine("Syksy");
+                    break;
+            default:
+                Console.WriteLine("Virheellinen numero!");
+                break;
+            } */
+            // C# 7 versiossa voidaan ilmaista myös tietty numero alue.
+            // ----------------------------------------------------------------------------------------  
+
+            // Ternary
+
+            Console.Write("Syötä kuukausi numerona: ");
+            int monthTernary = int.Parse(Console.ReadLine());
+
+            string message = "";
+
+            // ternary vertailu
+            message = monthTernary == 11 || monthTernary == 12 || monthTernary >= 1 && monthTernary <= 3 ? "Talvi" :
+                monthTernary >= 4 && monthTernary <= 5 ? "Kevät" :
+                monthTernary >= 6 && monthTernary <= 8 ? "kesä" :
+                monthTernary >= 9 && monthTernary <= 10 ? "Syksy" :
+                "Virheellinen arvo";
+
+
+            Console.WriteLine(message);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Opetuspäivä4harjotukset/Opetuspäivä4harjotukset/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opetuspäivä4harjotukset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region
+            
+            // 14 c# valinta: Parillinen vai pariton
+
+            // 1. Pyydetään käyttäjää antamaan luku
+            /* Console.Write("Anna luku: ");
+
+             // 2. Luetaan luku ja tallennetaa muuttujaan
+             int userNumber = int.Parse(Console.ReadLine());
+
+             // 3. Ehto rakenne, jossa verrataan muuttujan arvoa
+             // if-else
+             // %-operaatio, eli jakojäännös. Jos jakojäännös == 0, luku meni tasan kahdella.
+             if (userNumber % 2 == 0) // Onko jaollinen kahdella
+             {
+                 Console.WriteLine($"Numero {userNumber} on parillinen.");
+             }
+             //else if (userNumber % 2 == 1) tai
+             else 
+             {
+                 Console.WriteLine($"Numero {userNumber} on pariton.");
+             } 
+            // switch
+            // 1. Pyydetään käyttäjää antamaan luku
+            Console.Write("Anna luku: ");
+
+            /*
+            // 2. Luetaan luku ja tallennetaa muuttujaan
+            int userNumber = int.Parse(Console.ReadLine());
+
+            switch (userNumber % 2) // Suoritetaan jakojäännöksen laskeminen
+            {
+                case 0: // Verrataan jakojäännöksen tulosta. Jakojäännös on 0, eli parillinen
+                    Console.WriteLine($"Numero {userNumber} on parillinen. ");
+                    break;
+                case 1: // Jakojäännös jäi 1, eli pariton
+                    // default  // Voi käyttää myös defaulttia, koska luku on pariton, jos se ei ollut parillinen.
+                    Console.WriteLine($"Numero {userNumber} on pariton. ");
+                    break;
+            }
+            */
+            // ternary
+            // 1. Pyydetään käyttäjää antamaan luku
+            Console.Write("Anna luku: ");
+
+            // 2. Luetaan luku ja tallennetaa muuttujaan
+            int userNumber = int.Parse(Console.ReadLine());
+            //                      ehto           ?          arvojos true :                :          arvo jos false
+            string message = (userNumber % 2 == 0) ? $"Numero {userNumber} on parillinen. " : $"Numero {userNumber} on pariton. ";
+            //                                                                              Tässä kohtaa voi ketjuttaa uuden ternary operaation
+            Console.WriteLine("message"); // Näytetään tulos käyttäjälle
+
+
+
+
+
+            #endregion
+
+
+
+
+        }
+
+    }
+}
