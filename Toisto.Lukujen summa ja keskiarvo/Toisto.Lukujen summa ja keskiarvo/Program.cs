b4c66fa0faa7cd06cbc6877b8e096291179f0b5e
nasetin <sasuhujanen@outlook.com>
2023-04-04 19:59:03 +0300

    4.4 lähipv

--- Toisto.Lukujen summa ja keskiarvo/Toisto.Lukujen summa ja keskiarvo/Program.cs
+++ Toisto.Lukujen summa ja keskiarvo/Toisto.Lukujen summa ja keskiarvo/Program.cs
@@ -10,6 +10,35 @@ namespace Toisto.Lukujen_summa_ja_keskiarvo
     {
         static void Main(string[] args)
         {
+            //31B C# Toisto: Lukujen summa ja keskiarvo (Yhdessä päivä 5)
+
+            //Kirjoita ohjelma, joka kysyy käyttäjältä laskettavien lukujen määrän. Ohjelma pyytää lukuja annetun luvun verran.
+            //Lopussa sovellus näyttää lukujen summan ja keskiarvon.
+
+
+            Console.Write("Syötä laskettavien lukujen määrä: ");
+            int number = int.Parse(Console.ReadLine());
+            int i = 0;
+            double avg;
+            int sum = 0; // Luodaan summa muuttuja silmukan ulkopuolella, muuten muuttuja aina resetoituu.
+
+
+            for (i = 1; i <= number; i++)
+            {
+                Console.Write($"Syötä luku {i}: ");
+                // voi tehdä myös sum = sum + int.Parse(Console.ReadLine());
+                int input = int.Parse(Console.ReadLine());
+                sum += input;
+
+            }
+            avg = sum / number;
+            Console.Write($"Lukujen summa on {sum} ja lukujen keskiarvo on {avg}!");
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
