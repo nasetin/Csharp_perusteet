197696c6f9c6169c0d4d81694eab50a4c6f9cd78
nasetin <sasuhujanen@outlook.com>
2023-04-04 18:32:54 +0300

    4.4 lähipv

---
+++ 27 tähtien tulostus/27 tähtien tulostus/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_tähtien_tulostus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 27 C# Toisto: Tähtien tulostus (Yhdessä päivä 5)
+
+            // Tee ohjelma joka tulostaa halutun määrän tähtiä esim. 3 tulos on ***
+
+            // 1. Pyydetään käyttäjältä numero ja tallennetaan se muuttujaan
+            Console.Write("Syötä numero: ");
+            int number = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Syötä merkki: ");
+            char symbol = char.Parse(Console.ReadLine());
+
+            // 2. Lisätään silmukka, joka suoritetaan käyttäjän syöttämän numeron verran
+            // Tulostetaan yksi tähti, toistetaan käyttäjän syöttämän numeron verran
+            // while / do while
+            // for
+            // foreach
+
+            // Muokataan sovellus tulostamaan käyttäjän syöttämä merkki
+
+            // jos number = 5 ja i = 1;  1, 2, 3, 4, 5 => suoritetaan 5 kertaa
+            for(int i = 1; i <= number; i++)
+            {
+            Console.WriteLine(symbol);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tehtävä 28 parillinen vai pariton (4.4yhdessä)/tehtävä 28 parillinen vai pariton (4.4yhdessä)/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtävä_28_parillinen_vai_pariton__4._4yhdessä_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tee ohjelma joka kysyy mihin lukuun saakka tulostetaan lukuja.
+            // 20 => 1, 2, 3, 4, 5, 6, ....20
+            //Käyttäjältä myös kysytään, tulostetaanko parilliset vai parittomat.
+            // "Pariton"           "Parillinen"
+            //Sovellus lopuksi tulostaa joko parittomat tai parilliset luvut, nollasta alkaen.
+
+            // Aloitetaan tekemällä sovellus, joka tulostaa lukuja 1 - käyttäjän syöttämä luku
+
+            // 1. Pyydetään käyttäjältä luku
+            // 2. luetaan ja tallennetaan luku muuttujaan
+            //int number = int.Parse(Console.ReadLine());
+
+            // 3. Luodaan silmukka, joka suoritetaan niin monta kertaa kuin 1 - käyttäjä luku on
+            //            -while, pitää miettiä jokin sopiva ehto
+            //            -jotta voidaan tulostaa numeroita 1 alkaen, tarvitaan muuttuja, joka on alussa 1
+
+            //int i = 1; // iterointi: i++ => i++ => i == 3 jne
+
+            // Kysytään käyttäjältä halutaanko parillinen vai pariton
+            // Console.Write("Parillinen / Pariton: ");
+            // string input = Console.ReadLine();
+
+
+           /* while (i <= number)
+            {
+                // Lisätään ehto, joka tulostaa vain parittoman numeron
+                if (i % 2 != 0 && input == "Pariton")
+                {
+                    Console.WriteLine(i);
+                }
+                else if (i % 2 == 0 && input == "Parillinen")
+                {
+                    Console.WriteLine(i);
+                }
+                i++; // suoritetaan iteraatio jokaisella silmukan kierroksella
+            }
+
+            // 4. Silmukassa tulostetaan numero 1, jonka muuttuja iteroidaan joka kierros.
+           */
+
+            const string EVEN_TEXT = "Parillinen";
+            const string ODD_TEXT = "Pariton";
+
+            Console.Write("Syötä luku, johon asti tulostetaan: ");
+            int number = int.Parse(Console.ReadLine());
+
+            // while tarkistaa, että käyttäjä syöttää hyväksyttävän arvon
+            string input = ""; // Muuttuja luodaan ennen silmukkaa, jotta sitä voi käyttää ehdossa
+            bool inputFalse = true;
+
+            while (inputFalse == true) // silmukka toistetaan, niin kauan kuin käyttäjän input on väärin
+            {
+
+                Console.Write($"{EVEN_TEXT} / {ODD_TEXT} ");
+                input = Console.ReadLine();
+
+                if (input == EVEN_TEXT)
+                {
+                    inputFalse = false; // kun tämä muuttuja on false, pysähtyy silmukka
+                }
+                else if (input == ODD_TEXT)
+                {
+                    inputFalse = false; // kun tämä muuttuja on false, pysähtyy silmukka
+                }
+            }
+
+
+            // Käyttäjän syöte tarkistetaan aiemmin
+            int startingPoint = input == EVEN_TEXT ? 2 : 1;
+            // Parillinen alkaa 2
+            // Pariton alkaa 1
+
+            for (int i = startingPoint; i <= number; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
