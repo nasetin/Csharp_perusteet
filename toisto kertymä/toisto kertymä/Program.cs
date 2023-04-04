628bd1cbfb329887374887d1c260f429ffbe5973
nasetin <sasuhujanen@outlook.com>
2023-04-04 19:09:35 +0300

    4.4 lähipv

--- toisto kertymä/toisto kertymä/Program.cs
+++ toisto kertymä/toisto kertymä/Program.cs
@@ -10,6 +10,29 @@ namespace toisto_kertymä
     {
         static void Main(string[] args)
         {
+            // C# Toisto: Kertymä
+            // Tee ohjelma, joka pyytää käyttäjältä kierrosmäärän, ohjelma laskee kierroslukujen kertymän.
+            // Eli jos käyttäjä antaa syötteen 3, laskee ohjelma 0 + 1 + 2, jos 5 niin 0 + 1 + 2 + 3 + 4.
+            // Lopuksi ohjelma tulostaa käyttäjälle lopputuloksen muodossa:
+            // 3 kierrosta Kertymäksi saatiin: 3
+
+            // Toteuta käyttämällä for rakennetta
+            // Toteuta myös harjoitukseksi while rakenteella
+
+            Console.Write("Syötä kierrosmäärä: ");
+            int loops = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            // Summataan luvut o:sta käyttäjän lukuun asti
+            for (int i = 0; i < loops; i++)
+            {
+                // Summataan, tarvitaan muuttuja
+                sum = sum + i;
+            }
+
+            Console.WriteLine($"{loops} kierrosta kertymäksi saatiin: {sum}");
+
+            Console.ReadKey();
         }
     }
 }
