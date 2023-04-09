289127f9b8045f1ea0b73d9dd2fc6781de1aa468
nasetin <sasuhujanen@outlook.com>
2023-04-09 08:06:00 +0300

    kotitehtäviä

---
+++ Csharp Salasana if else ja while/Csharp Salasana if else ja while/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Salasana_if_else_ja_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "salasana123";
+            bool passwordCorrect = false;
+
+            while (passwordCorrect == false)
+            {
+
+                Console.Write("Syötä salasan: ");
+                string userinput = Console.ReadLine();
+
+                if (userinput == password)
+                {
+                    Console.WriteLine("Salasana oikein!!!");
+                    passwordCorrect = true;
+                }
+                else
+                {
+                    Console.WriteLine("Salasana väärin:!!!");
+                }
+            }
+        }
+    }
+}
