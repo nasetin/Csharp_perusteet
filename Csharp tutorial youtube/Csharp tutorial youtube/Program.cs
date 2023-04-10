d61037ce424fc05a6434ba30a33607ee269891b9
nasetin <sasuhujanen@outlook.com>
2023-04-10 07:17:29 +0300

    yt tutotial

---
+++ Csharp tutorial youtube/Csharp tutorial youtube/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_tutorial_youtube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Drawing a Shape - Piirretään kuvia
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            // Variables - Muuttujat
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
