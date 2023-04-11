7de073e5fffac2480f1335d828d2bd82cb8f2401
nasetin <sasuhujanen@outlook.com>
2023-04-11 18:29:33 +0300

    lähipv11.4

---
+++ 33 Csharp String Merkkien tulostus/33 Csharp String Merkkien tulostus/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33_Csharp_String_Merkkien_tulostus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tee ohjelma joko tulostaa sanan alusta tai lopusta halutun määrän merkkejä.
+            // Käyttäjä valitse tulostetaanko merkit sanan alusta vai lopusta.
+            // Pyydetään sana ja tulostun suunta yhdellä input: lla.
+
+            // Esimerkki käyttäjän syötteestä: "Lattia A" "Seinä L"
+
+            // Merkki " - voidaan näyttää käyttäjälle kenoviivalla \ ennen "-merkkiä. ctrl+alt+?
+            Console.Write("Syötä sana ja tulostus suunta (\"Lattia A\"): ");
+            string input = Console.ReadLine();
+            string[] inputSplit = input.Split(' ');
+            // Käyttäjä syötti "terve L"
+            // inputSplit[0] == "terve"
+            // inputSplit[1] == "L"
+
+            // Tarvitaan valintarakenne, jolla tutkitaan tulostuksen suuntaa
+            // Ehtorakenne kääntää string merkkien järjestyksen
+            if (inputSplit[1] == "A") // Alusta
+            {
+                // Tätä ei periaatteessa tarvita
+            }
+            else if (inputSplit[1] == "L") // Lopusta
+            {
+                char[] chars = inputSplit[0].ToCharArray(); 
+                Array.Reverse(chars);
+                string test = new string(chars);
+                inputSplit[0] = test;
+            }
+
+            foreach (char jokinNimi in inputSplit[0])
+            {
+                Console.WriteLine(jokinNimi);
+            }
+
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Päivän6harjoitus/Päivän6harjoitus/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Päivän6harjoitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // c# for ja switch
+           
+            double initialValue = 12d;
+
+            // Luodaan for silmukka, joka tapahtuu 4 kertaa
+            // 1 - 2 - 3 - 4 
+            for (int i = 1; i < 5; i++ )
+            {
+                switch (i) // sULKUJEN SISÄLLÄ TUTKITTAVA ARVO
+                {
+                    case 1:
+                        // Koodi
+                        initialValue += i;
+                        break; // Päättää switch rakenteen suorituksen
+                    case 2:
+                        initialValue = initialValue * i;
+                        break;
+                    case 3:
+                        initialValue /= i;
+                        break;
+                    case 4:
+                        initialValue = initialValue - i;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Lopputulos: {initialValue}");
+            
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
