b41be6e7220a9007bfb7b07f53a1dffcad244fcc
nasetin <sasuhujanen@outlook.com>
2023-04-12 17:02:54 +0300

    ytharjotus

--- Csharp tutorial youtube/Csharp tutorial youtube/Program.cs
+++ Csharp tutorial youtube/Csharp tutorial youtube/Program.cs
@@ -11,15 +11,49 @@ namespace Csharp_tutorial_youtube
         static void Main(string[] args)
         {
             // Drawing a Shape - Piirretään kuvia
-
+            /*
             Console.WriteLine("Hello World!");
 
             Console.WriteLine("   /|");
             Console.WriteLine("  / |");
             Console.WriteLine(" /  |");
             Console.WriteLine("/___|");
+            Console.WriteLine();
+            */
 
             // Variables - Muuttujat
+            /*
+            string characterName = "Tom";
+            int characterAge;
+            characterAge = 25;
+
+            Console.WriteLine("There ince was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+
+            characterName = "Mike";
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+            Console.WriteLine();
+            */
+
+            // Data Types
+            /*
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            double gpa = 3.2;
+            bool isMale = true;
+
+            Console.WriteLine(false);
+            Console.WriteLine();
+            */
+
+            // Working with strings
+            // \n \"
+            string phrase = "Giraffe Academy";
+
+            Console.WriteLine(phrase);
+
 
 
 
