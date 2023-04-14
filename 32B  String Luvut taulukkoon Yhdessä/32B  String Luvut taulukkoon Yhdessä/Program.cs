4c975c0029730725e4e63c4a95a9c123c5cb92a1
nasetin <sasuhujanen@outlook.com>
2023-04-14 05:36:37 +0300

    12.4

---
+++ 32B  String Luvut taulukkoon Yhdessä/32B  String Luvut taulukkoon Yhdessä/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32B__String_Luvut_taulukkoon_Yhdessä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Luetaan käyttäjältä lukuja. Tallennetaan luvut taulukkoon talteen.
+            // Lopuksi summataan taulukossa olevat arvot.
+            // Toteuta sama asia, mutta käyttämällä listaa.
+
+            //int[] userValues = new int[3];
+
+            //// Silmukka, jossa pyydetään 3 arvoa- 1 per kierros.
+            //// Silmukan ehto on se, että päätetään suoritus silloin,
+            //// kun i-muuttujan arvo on taulukon pituuden ulkopuolella
+            //// Jos pituus on 3, indeksit ovat 0 1 2
+
+            //Console.WriteLine("Syötä 3 lukua: ");
+
+            //for (int i = 0; i < userValues.Length; i++)
+            //{
+            //    userValues[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //// Myöhemmin tarvitaan summa
+
+            //Console.WriteLine($"Lukujen summa on: {userValues.Sum()}");
+
+            //int sum = 0;
+
+            //// Silmukka, jossa summataan kaikkien indeksien arvot.
+            //for (int i = 0; i < userValues.Length; i++)
+            //{
+            //    sum = sum + userValues[i];
+            //}
+
+            //Console.WriteLine($"Lukujen summa on: {sum}");
+
+            // Lista versio
+            // Tässä versiossa käyttäjä voi tallentaa niin monta lukua kuin haluaa
+
+            Console.WriteLine("Syötä lukuja (e to exit): ");
+            List<int> values = new List<int>();
+            bool userIsDone = false;
+
+            // Silmukka, jossa käyttäjä voi syöttää haluamansa verran lukuja.
+            while (userIsDone == false) // tai do while
+            {
+                string userInput = Console.ReadLine();
+
+                if (userInput == "e")
+                {
+                    userIsDone = true;
+                }
+                else // Muuten lisätään luku listaan.
+                {
+                    values.Add(int.Parse(userInput));
+                }
+            }
+
+            // Listan summaus
+            Console.WriteLine($"Listan lukujen summa on: {values.Sum()}");
+
+            int sumList = 0;
+            foreach (int number in values)
+            {
+                sumList += number;
+            }
+
+            Console.WriteLine($"Listan lukujen summa on: {sumList}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 34D  String Etsi kirjaimia merkkijonosta/34D  String Etsi kirjaimia merkkijonosta/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34D__String_Etsi_kirjaimia_merkkijonosta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Syötä sana 1: ");
+            string word1 = Console.ReadLine(); // "kuppi"
+
+            Console.Write("Syötä sana 2: ");
+            string word2 = Console.ReadLine(); // "kauppa"
+
+            // Taulukon voi tietää kuinka pitkä sana on
+            // 1. Ei voi tietää kuinka pitkä sana on
+            // 2. ei voi tietää montako merkkiä ovat samoja
+
+            // TODO: Luo taulukon pituus tarkalleen oikein.
+
+            //charsInCommon sisältää merkit, jotka ilmenevät molemmissa ja montako kertaa ne ilmenee
+            // Tässä datatyyppi on "Tuple" johon voi tallentaa kaksi eri datatyyppiä yhdessä
+            (char charValue, int intValue)[] charsInCommon = new (char, int)[word1.Length];
+
+            string charTested = "";
+
+            // Luodaan silmukka, joka käy läpi kaikki word1 merkit ja tarkistetaan se word2-muuttujassa
+            for (int i = 0; i < word1.Length; i++) // word1 indeksi  == i
+            {
+                int numberOfTimesFound = 0;
+                for (int j = 0; j < word2.Length; j++) // word2 indeksi == j
+                {
+                    // Onko sanan i sama kirjain kuin sanan 2 indeksissä j
+                    // Ja ei ole vielä tallennettu kirjainta taulukkoon
+                    if (word1[i] == word2[j] && charTested.Contains(word1[i]) == false) 
+                    {
+                        // Estetään saman kirjaimen tallennus uudestaan
+
+                        numberOfTimesFound++;
+                        charsInCommon[i] = (word1[i], numberOfTimesFound);
+                    }
+                }
+                // Lisätään löydetty merkki, vasta sisemmän silmukan jälkeen
+
+                charTested += word1[i];
+            }
+            // Silmukka, jossa käydään läpi taulukko löydetyistä kirjaimista
+            foreach( (char,int) pair in charsInCommon)
+            {
+                Console.WriteLine($"Merkki {pair.Item1} löytyi {pair.Item2} kerran/kertaa");
+            }
+            Console.ReadKey();
+        } 
+    }
+}
