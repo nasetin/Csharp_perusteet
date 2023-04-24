39937c52a0e43096f4284920b273bdcbb1849723
nasetin <sasuhujanen@outlook.com>
2023-04-24 05:53:36 +0300

    C# yt harjotus 3/4 kesken

---
+++ Csharp tutorial youtube 3!4/Csharp tutorial youtube 3!4/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_tutorial_youtube_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // C# tutorial youtube 3/4
+
+            // Guessing game
+
+            //string secretWord = "giraffe";
+            //string guess = "";
+            //int guessCount = 0;
+            //int guessLimit = 3;
+            //bool outOfGuesses = false;
+
+            //while (guess != secretWord && !outOfGuesses)
+            //{
+            //    if (guessCount < guessLimit)
+            //    {
+            //        Console.Write("Enter guess: ");
+            //        guess = Console.ReadLine();
+            //        guessCount++;
+            //    }
+            //    else
+            //    {
+            //        outOfGuesses = true;
+            //    }
+
+            //}
+            //if (outOfGuesses)
+            //{
+            //    Console.Write("You lose!");
+            //}
+            //else
+            //{
+
+            //    Console.Write("You win!");
+            //}
+
+
+            // For Loops_________________________________________
+
+            //int i = 1;
+            //while (i <= 5)
+            //{
+            //Console.WriteLine(i);
+            //i++;
+            //}
+
+            //int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+
+            //for(int i = 0; i < luckyNumbers.Length; i++)
+            //{
+            //    Console.WriteLine(luckyNumbers[i]);
+
+            //}
+
+            // Buildin an exponent method______________________________
+
+
+            // Console.WriteLine(GetPow(3, 2));
+
+            // 2d Arrays_______________________________________
+
+            //int[,] numberGrid =
+            //{
+            //    {1, 2},
+            //    {3, 4},
+            //    {5, 6},    
+
+            //};
+            //int[,] myArray = new int[2, 3];
+
+            //Console.WriteLine(numberGrid[0, 0]);
+
+            // Comments___________________________________________
+
+            /*
+             lol
+            lol
+
+            Console.WriteLine("Comments are fun");
+             */
+
+            // Exception handling
+
+
+            Console.ReadKey();
+        }
+
+        //static int GetPow(int baseNum, int powNum)
+        //{
+        //    int result = 1;
+
+        //    for (int i = 0; 1 < powNum; i++)
+        //    {
+        //        result = result * baseNum;
+        //    }
+
+
+        //    return result;
+        //}
+
+    }
+}
---
+++ Csharp tutorial youtube 3!4/Csharp tutorial youtube 3!4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Csharp tutorial youtube 3!4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Csharp tutorial youtube 3!4")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("83c59135-2afc-416e-a0e8-6d52f9aa2cef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
