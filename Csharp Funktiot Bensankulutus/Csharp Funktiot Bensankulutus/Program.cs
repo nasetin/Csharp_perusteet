ef181080632816d547fc08dc11a639ce3d875414
nasetin <sasuhujanen@outlook.com>
2023-04-24 18:43:16 +0300

    PA-kulutus funktio

--- Csharp Funktiot Bensankulutus/Csharp Funktiot Bensankulutus/Program.cs
+++ Csharp Funktiot Bensankulutus/Csharp Funktiot Bensankulutus/Program.cs
@@ -11,24 +11,60 @@ namespace Csharp_Funktiot_Bensankulutus
         static void Main(string[] args)
         {
             // C# Funktiot: Bensankulutus
+            bool userEndsLoop = false;
 
-            Console.Write("Syötä ajettu matka(km): ");
-            decimal distance =  decimal.Parse(Console.ReadLine());
+            // List<(decimal, decimal, decimal)> test = new List<(decimal, decimal, decimal)>(); // Tuple
+            List<decimal> userDistance = new List<decimal>();
+            List<decimal> userAverageConsumption = new List<decimal>();
+            List<decimal> userFuelPrice = new List<decimal>();
+            // Kun käytetään useaa eri listaa tai taulua, täytyy hallita indeksien käyttö oikein eri listojen välillä
 
-            Console.Write("Syötä ajoneuvon keskikulutus(1/100km): ");
-            decimal averageConsumtion = decimal.Parse(Console.ReadLine());
+            decimal[] test = new decimal[1];
+            (decimal, decimal, decimal)[] trips = new (decimal, decimal, decimal)[1];
+
+            while (userEndsLoop == false)
+            {
+
+                Console.Write("Syötä ajettu matka(km): ");
+                decimal distance = decimal.Parse(Console.ReadLine());
+                userDistance.Add(distance);
+
+                Console.Write("Syötä ajoneuvon keskikulutus(1/100km): ");
+                decimal averageConsumtion = decimal.Parse(Console.ReadLine());
+                userAverageConsumption.Add(averageConsumtion);
 
 
-            Console.Write("Syötä polttoaineen hinta(€/1): ");
-            decimal fuelPrice = decimal.Parse(Console.ReadLine());
+                Console.Write("Syötä polttoaineen hinta(€/1): ");
+                decimal fuelPrice = decimal.Parse(Console.ReadLine());
+                userFuelPrice.Add(fuelPrice);
 
-            decimal tripCost = carTripCostCalculator(distance, averageConsumtion, fuelPrice);
+                trips[trips.Length - 1] = (distance, averageConsumtion, fuelPrice);
+                    
 
-            Console.WriteLine();
-            Console.WriteLine($"Ajetun matkan hinta on:  {tripCost} euroa");
+                // decimal tripCost = carTripCostCalculator(distance, averageConsumtion, fuelPrice);
 
+                // Console.WriteLine();
+                // Console.WriteLine($"Ajetun matkan hinta on:  {tripCost} euroa");
+                // Console.WriteLine();
 
-            Console.WriteLine("Press any key to exit: ");
+                Console.Write("Calculate another trip? (y/n): ");
+                string input = Console.ReadLine();
+
+                if (input == "n") // Käyttäjä lopettaa sovelluksen
+                {
+                    userEndsLoop = true; // Kun tämä muuttuja on true, silmukka päättyy
+                }
+                else
+                {
+                    trips = expandArray(trips); // expandArray palauttaa uuden taulukon, joka on yhtä suurempi kuin alkuperäinen
+                }
+            }
+
+            // Käydään listat läpi ja tulostetaan matkojen hinnat
+            for (int i = 0; i < userDistance.Count; i++)
+            {
+                Console.WriteLine($"Matkan {i+1} hinta on: {carTripCostCalculator(userDistance[i], userAverageConsumption[i], userFuelPrice[i])}");
+            }
             Console.ReadKey();
         }
         // Parametrien yksiköt joko kommentoidaan tai lisätään parametrien nimiin.
@@ -40,6 +76,19 @@ namespace Csharp_Funktiot_Bensankulutus
 
 
             return result;
+        }
+        private static (decimal, decimal, decimal)[] expandArray((decimal, decimal, decimal)[] originalArray)
+        {
+            // Luodaan uusi taulukko, joka on yhtä suurempi kuin alkuperäinen taulukko
+            (decimal, decimal, decimal)[] newArray = new (decimal, decimal, decimal)[originalArray.Length + 1];
+
+            // Kopioidaan arvot vanhasta taulukosta uuteen taulukkoon
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                newArray[i] = originalArray[i];
+            }
+
+            return newArray;
         }
 
     }
