10e36b1ffd93f01c2c77d904d095a7e2572c0f6b
nasetin <sasuhujanen@outlook.com>
2023-04-25 05:46:02 +0300

    update

---
+++ Csharp Funktiot Lämpötilan muunnin/Csharp Funktiot Lämpötilan muunnin/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Funktiot_Lämpötilan_muunnin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // C# Funktiot: Lämpötilan muunnin
+            // C = (5/9) * (F-32)
+            // F = C * 9 / 5 + 32
+
+            
+            bool userIsDone = false;
+            while (userIsDone == false)
+            {
+                // DOTO:
+                // kaatuu jos syöttää kirjaimia
+            Console.Write("Syötä Lämpötila: ");
+            double temperature = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Muuta antamasi lämpötila Fahrenheit asteiksi valitsemalla f.");
+            Console.WriteLine("Muuta antamasi lämpötila celcius asteiksi valitsemalla: c.");
+            string choice = Console.ReadLine();
+
+            if (choice == "f") // fahrenheit!!!!!!!!!!
+            {
+
+                Console.WriteLine($"Lämpötila on {celsiusToFahrenheit(temperature)} F");
+                    userIsDone = true;
+            }
+
+            else if (choice == "c") // celsius!!!!!!
+            {
+                Console.WriteLine($"Lämpötila on {fahrenheitToCelsius(temperature)} C");
+                    userIsDone = true;
+            }
+
+            else
+            {
+                Console.WriteLine("Väärä syöte kokeile uudestaan");
+                Console.WriteLine("f = Fahrenheit");
+                Console.WriteLine("c = Celsius");
+
+                Console.WriteLine();
+            }
+
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+
+        private static double fahrenheitToCelsius(double temperature)
+        {
+
+            double celsius = (5 / 9d) * (temperature - 32);
+            return celsius;
+        }
+
+        private static double celsiusToFahrenheit(double temperature)
+        {
+            double fahrenheit = temperature * 9 / 5 + 32;
+
+            return fahrenheit;
+        }
+    }
+}
---
+++ Csharp Funktiot Lämpötilan muunnin/Csharp Funktiot Lämpötilan muunnin/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Csharp Funktiot Lämpötilan muunnin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Csharp Funktiot Lämpötilan muunnin")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("871532ba-2436-431a-9744-6682472653ff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
