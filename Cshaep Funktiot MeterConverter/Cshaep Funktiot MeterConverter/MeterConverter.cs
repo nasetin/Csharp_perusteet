dd354d5056d059d6461437d223e5893155758c8c
nasetin <sasuhujanen@outlook.com>
2023-05-02 17:41:53 +0300

    Meter converter (kesken...)

---
+++ Cshaep Funktiot MeterConverter/Cshaep Funktiot MeterConverter/MeterConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cshaep_Funktiot_MeterConverter
+{
+    class MeterConverter
+    {
+        // Properties
+
+        // Construcyor
+
+        // Methods
+
+        public static double ToCentimeter(double meters)
+        {
+            return meters * 100;
+        }
+
+        public static double ToKilometer(double meters)
+        {
+            return meters / 1000;
+        }
+
+        // Loput metodit....
+
+
+
+    }
+}
---
+++ Cshaep Funktiot MeterConverter/Cshaep Funktiot MeterConverter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cshaep_Funktiot_MeterConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Syötä mitta(m): ");
+            double meters = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{meters}m sentteinä on:{MeterConverter.ToCentimeter(1.826)};");
+            Console.WriteLine($"{meters}m kilometreinä on:{MeterConverter.ToKilometer(meters)}");
+
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
