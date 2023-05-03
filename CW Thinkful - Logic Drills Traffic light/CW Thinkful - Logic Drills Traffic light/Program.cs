6268c44054bce12d96af6ce7e989f4ed12b0ab8e
nasetin <sasuhujanen@outlook.com>
2023-05-03 19:54:05 +0300

    CW_Thinkful___Logic_Drills_Traffic_light

---
+++ CW Thinkful - Logic Drills Traffic light/CW Thinkful - Logic Drills Traffic light/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_Thinkful___Logic_Drills_Traffic_light
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+        public static string UpdateLight(string current)
+        {
+            string result = "";
+            //Do Some Magic
+            switch (current)
+            {
+                case "green":
+                    result = "yellow";
+                    break;
+
+                case "yellow":
+                    result = "red";
+                    break;
+                case "red":
+                    result = "green";
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ festa od the beasts cw/festa od the beasts cw/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace festa_od_the_beasts_cw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
