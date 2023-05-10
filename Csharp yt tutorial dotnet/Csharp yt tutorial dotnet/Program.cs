301a32473cfb043df8ccd17a85da610eaf6bd4b3
nasetin <sasuhujanen@outlook.com>
2023-05-10 16:09:00 +0300

    kesken

---
+++ Csharp yt tutorial dotnet/Csharp yt tutorial dotnet/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_yt_tutorial_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string aFriend = "italian chef";
+            //string firstFriend = "Nase";
+            //string secondFriend = "Mike";
+            //Console.WriteLine($"My friends are {aFriend} and {firstFriend} and {secondFriend}");
+
+            //Console.WriteLine($"The name {aFriend} has {aFriend.Length} letters. ");
+            //Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters. ");
+            //Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters. ");
+
+            //string greeting = "     Hello world!     ";
+            //Console.WriteLine($"***{greeting}***");
+
+            //string trimmedGreeting = greeting.TrimStart();
+            //Console.WriteLine($"[{trimmedGreeting}]");
+
+            //trimmedGreeting = greeting.TrimEnd();
+            //Console.WriteLine($"[{trimmedGreeting}]");
+
+            //trimmedGreeting = greeting.Trim();
+            //Console.WriteLine($"[{trimmedGreeting}]");
+
+            //string sayHello = "Hello world!";
+            //Console.WriteLine(sayHello);
+
+            //sayHello = sayHello.Replace("Hello", "Greetings");
+            //Console.WriteLine(sayHello);
+
+            //Console.WriteLine(sayHello.ToUpper());
+            //Console.WriteLine(sayHello.ToLower());
+
+            //string songLyrics = "You say greetings, and I say hello";
+
+            //var result = songLyrics.StartsWith("You");
+            //Console.WriteLine(result);
+            //Console.WriteLine(songLyrics.Contains("greetings"));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
