b96aa8b86d5075f2c3c3feae75182ec04f44c7ab
nasetin <sasuhujanen@outlook.com>
2023-05-15 20:04:23 +0300

    vaiheessa

--- funktio ja luokka kertausta/funktio ja luokka kertausta/Program.cs
+++ funktio ja luokka kertausta/funktio ja luokka kertausta/Program.cs
@@ -10,17 +10,50 @@ namespace funktio_ja_luokka_kertausta
     {
         static void Main(string[] args)
         {
-            Dice noppa1 = new Dice(6);
-            Dice noppa2 = new Dice(6, 1);
+            // NOPPAPELI_____________________________________
+            //Dice noppa1 = new Dice(6);
+            //Dice noppa2 = new Dice(6, 1);
 
-            noppa1.Roll();
-            //Console.ReadKey();
-            noppa2.Roll();
+            //noppa1.Roll();
+            ////Console.ReadKey();
+            //noppa2.Roll();
 
-            Console.WriteLine($"Nopan 1 silmäluku on: {noppa1.Value}");
-            Console.WriteLine($"Nopan 2 silmäluku on: {noppa2.Value}");
+            //Console.WriteLine($"Nopan 1 silmäluku on: {noppa1.Value}");
+            //Console.WriteLine($"Nopan 2 silmäluku on: {noppa2.Value}");
             // Tallennetaan erikseen data objektiin
             // null reference on yleinen sovelluksen kaatumiseen
+
+            //KAUPPALISTA__________________________________________________________
+
+            //List<ShoppingList> order = new List<ShoppingList>();
+
+            Console.Write("Mihin kauppaan tehdään ostoslista: ");
+            string shop = Console.ReadLine();
+
+            ShoppingList newOrder = new ShoppingList(shop);
+
+            // Suunnittelun tapa menetelmät
+            // Waterfall - Mini/Agile waterfall(scrum) - Agile(pari ohjelmointi)
+
+            do // Käyttäjä lisää ostoksia
+            {
+                Console.Clear();
+                Console.WriteLine($"Valittu kauppa on {newOrder.ShopName}");
+                Console.WriteLine();
+
+                Console.Write("Syötä tuote ja lukumäärä(esim. maito, 3): ");
+                string input = Console.ReadLine();
+                string[] splitInput = input.Split(',');
+                int ammount = int.Parse(splitInput[1].Trim());
+
+                //Lisätään tuote listaan käyttäjän syöttämän luvun verran
+                for (int i = 1; i <= ammount; i++)
+                {
+                    newOrder.AddProduct(splitInput[0]);
+                }
+
+
+            } while (true); // TODO this condition
 
             Console.ReadKey();
         }
---
+++ funktio ja luokka kertausta/funktio ja luokka kertausta/ShoppingList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funktio_ja_luokka_kertausta
+{
+    // Voidaan luoda ostoslistoja eri kaupoille
+    // Tuote lisätessä listaan "haetaan" tuotteen hinta.
+    class ShoppingList
+    {
+        // Properties
+        public string ShopName { get; set; }
+
+        private List<Product> Products { get; set; }
+
+        public decimal Sum { get; set; }
+
+        // Constructor
+
+        public ShoppingList(string shopName)
+        {
+            ShopName = shopName;
+            Sum = 0;
+            Products = new List<Product>();
+        }
+
+        // Methods
+        public void AddProduct(string productName)
+        {
+            Product temp = new Product(productName);
+            Products.Add(temp);
+            Sum += temp.Price; 
+        }
+    }
+}
