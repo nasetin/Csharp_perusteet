ef01639598330a01e51bf20fd25b702737387f07
nasetin <sasuhujanen@outlook.com>
2023-05-23 20:18:57 +0300

    huh huh

---
+++ Csharp 23.5. koe/Csharp 23.5. koe/JavelinThrow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_23._5.koe
+{
+    class JavelinThrow
+    {
+        public int Id { get; set; }
+
+        public string AthleteName { get; set; }
+
+        public int Meters { get; set; }
+
+        public int Centimeters { get; set; }
+
+        public JavelinThrow(int id, string athleteName, int meters, int centimeters)
+        {
+            Id = id;
+            AthleteName = athleteName;
+            Meters = meters;
+            Centimeters = centimeters;
+        }
+
+
+    }
+    
+    }
+
+
+
+    
+
---
+++ Csharp 23.5. koe/Csharp 23.5. koe/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_23._5.koe
+{
+    class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Product(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+
+        }
+
+
+    }
+}
